0713600fe91cde68e3e915b96d8044be72a1e8c9
Rajib <ahmedrajib942@gmail.com>
2019-10-19 15:11:44 +0600

    upate

---
+++ Assets/SliderTarget.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SliderTarget : MonoBehaviour
+{
+    private float time = 30;
+    private Slider slide;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /*IEnumerator Target()
+    {
+        
+       // yield return new WaitForSeconds(anim1.GetCurrentAnimatorStateInfo(0).length);
+        
+    }*/
+}
